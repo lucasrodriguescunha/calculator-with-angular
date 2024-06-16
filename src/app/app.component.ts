296a8501c1c68166d3363da31dae3f064e984310
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-06-16 16:28:27 -0300

    chore: Update calculator component layout and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,41 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  // @ViewChild('result') resultElement!: ElementRef; permite que você acesse diretamente o elemento HTML no qual você adicionou #result. Isso é mais seguro e idiomático do que usar document.getElementById.
+  @ViewChild('result') resultElement!: ElementRef;
+
+  // Adiciona o número ao conteúdo existente no elemento
+  insert(num: string): void {
+    const resultElement = this.resultElement.nativeElement;
+    resultElement.innerHTML += num;
+  }
+
+  // Limpa o conteúdo do elemento
+  clean(): void {
+    this.resultElement.nativeElement.innerHTML = '';
+  }
+
+  // Remove o último caractere do conteúdo do elemento
+  back(): void {
+    const resultElement = this.resultElement.nativeElement;
+    const result = resultElement.innerHTML;
+    resultElement.innerHTML = result.slice(0, -1);
+  }
+
+  // Avalia a expressão matemática no conteúdo do elemento e exibe o resultado
+  calculate(): void {
+    const resultElement = this.resultElement.nativeElement;
+    const result = resultElement.innerHTML;
+
+    if (result) {
+      try {
+        resultElement.innerHTML = eval(result);
+      } catch (error) {
+        resultElement.innerHTML = 'Erro';
+      }
+    } else {
+      resultElement.innerHTML = 'Nada';
+    }
+  }
+}
